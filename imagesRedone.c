a0ffb752933569c3bc32e800ffa30761728b8327
Brayton H Arthur <braytona@bullseye-01312022>
2024-05-06 16:30:12 -0700

    Progress

--- imagesRedone.c
+++ imagesRedone.c
@@ -3,7 +3,7 @@
 //Purpose: Final Project
 
 #include <stdio.h>
-#include <stdbool.h>
+
 
 #define MAX_SIZE 1000
 
@@ -23,7 +23,7 @@ void saveImage();
 
 int main(){
 	int choice;
-	while(choice != 0){
+	do{
 		displayMenu();
 		printf("Choose from one of the options above: ");
 		scanf("%d", &choice);
@@ -44,7 +44,7 @@ int main(){
 				printf("Invalid.\n");
 				break;
 		}
-	}
+	}while(choice != 0);
 	
 	return 0;
 }
@@ -63,7 +63,12 @@ void loadImage(){
 	scanf("%s", filename);
 
 	FILE *file = fopen(filename, "r");
-	
+	for(height = 0; height < MAX_SIZE; height++){
+		for(width = 0; width < MAX_SIZE; width++){
+			fscanf(file, "%d", &pixels[height][width]);
+		}
+	}
+
 	
 	
 	if (file == NULL) { 
@@ -77,12 +82,105 @@ void loadImage(){
 void displayImage(){
 	printf("\nImage:\n");
 	for(height = 0; height < MAX_SIZE; height++){
-		printf("%c", pixels[height]);
-	}
+		for(width = 0; width < MAX_SIZE; width++){
+			if(pixels[height][width] == 0){
+				printf(" ");
+			}
+			if(pixels[height][width] == 1){
+				printf(".");
+			}
+			if(pixels[height][width] == 2){
+				printf("o");
+			}
+			if(pixels[height][width] == 3){
+				printf("O");
+			}
+			if(pixels[height][width] == 4){
+				printf("0");
+			}
+			else{
+				break;
+			}
+			/*switch(pixels[height][width]){
+				case 0:
+					printf(" ");
+					break;
+				case 1:
+					printf(".");
+					break;	
+				case 2:
+					printf("o");
+					break;
+				case 3:
+					printf("O");
+					break;	
+				case 4:
+					printf("0");
+					break;
+				default:
+					break;
+					
+			}*/
+		}
+	}	
 }	
 	
+void editImage() {
+	int editChoice;
+	printf("\n**EDITING**\n");
+	printf("1. Crop image\n");
+	printf("2. Dim image\n");
+	printf("3. Brighten image\n");
+	printf("4. Save image\n");
+	printf("0. Back to main menu\n");
+	printf("Enter your choice: ");
+	scanf("%d", &editChoice);
 	
+	switch (editChoice) {
+		case 1:
+			cropImage();
+			break;
+		case 2:	
+			dimImage();
+			break;
+		case 3:	
+			brightenImage();
+			break;
+		case 4:	
+			saveImage();
+			break;
+		case 0:	
+			printf("Returning to main menu...\n");
+			break;
+		default:	
+			printf("Invalid.\n");
+			break;
+	}
+}
+
+void cropImage() {
+}
 	
+void dimImage() {
+/*
+	for(height = 0; height < MAX_SIZE; height++){
+		for(width = 0; width < MAX_SIZE; width++){
+			if(pixels[height][width] == "o"){
+				pixels[height][width] = "O"
+			
+			}
+		}
+	}
+*/	
+}
+
+void brightenImage() {
+}
+
+void saveImage() {
+}
+		
+			
 	
 	
 	
